61bd5864d9438f46e1ae7c3407b95878ea7f6d93
marco-kraemer <marco.k.santos@hotmail.com>
2021-03-21 18:47:28 -0300

    Update

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 11:24:32 by maraurel          #+#    #+#             */
-/*   Updated: 2021/03/21 17:28:08 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/03/21 18:47:05 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,32 @@
 
 typedef struct		s_flags
 {
-	int		width;
-	int		precision;
-	int		k;
+	int			width;
+	int			precision;
+	int			k;
 }					t_flags;
 
-int		ft_printf(const char *s, ...);
-int		print_string(va_list ap, char *saved);
-int		print_integer(va_list ap, char *saved);
-int		print_hex(va_list ap, char *saved, char type);
-int		get_precision(va_list ap, char *saved);
-int		get_length(va_list ap, char *saved);
-int		is_flag(const char *saved, int i);
-int		print_char(va_list ap, char *saved);
-int		print_address(va_list ap, char *saved);
-int		print_percent(va_list ap, char *saved);
-int		print_unsigned(va_list ap, char *saved);
-int		conversions(const char *s, int i);
-int		is_length(const char *s, int i);
-int		treat_int_0_negative(char *tmp, char *print, t_flags flags, int i);
-char	*ft_itoa_unsigned(unsigned int n);
-char	*malloc_print_integer(size_t length, char *tmp, int precision, int function);
-char	*is_negative(char *tmp, int j);
-char	*get_hex(unsigned int i, char type);
-char	*get_address(unsigned long long i, int function);
+int					ft_printf(const char *s, ...);
+int					print_string(va_list ap, char *saved);
+int					print_integer(va_list ap, char *saved);
+int					print_hex(va_list ap, char *saved, char type);
+int					get_precision(va_list ap, char *saved);
+int					get_length(va_list ap, char *saved);
+int					is_flag(const char *saved, int i);
+int					print_char(va_list ap, char *saved);
+int					print_address(va_list ap, char *saved);
+int					print_percent(va_list ap, char *saved);
+int					print_unsigned(va_list ap, char *saved);
+int					conversions(const char *s, int i);
+int					is_length(const char *s, int i);
+int					treat_int_0_negative(char *tmp, char *print,
+		t_flags flags, int i);
+
+char				*ft_itoa_unsigned(unsigned int n);
+char				*malloc_print_integer(size_t length, char *tmp,
+		int precision, int function);
+char				*is_negative(char *tmp, int j);
+char				*get_hex(unsigned int i, char type);
+char				*get_address(unsigned long long i, int function);
 
 #endif
